4aa926945737228ed142cfe6b4221c70e418c3c1
hbk1087 <hansarianeil@gmail.com>
2023-10-19 20:46:17 -0400

    fd data

--- backend/odds.py
+++ backend/odds.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 secret_key = os.getenv('API_KEY')
 # Replace this URL with the actual API URL you want to call
-api_url = f"https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds/?apiKey={secret_key}&regions=us&markets=h2h,spreads,totals&oddsFormat=american"
+api_url = f"https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds/?apiKey={secret_key}&regions=us&markets=h2h,spreads,totals&oddsFormat=american&bookmakers=fanduel"
 print(api_url)
 # Send a GET request to the API
 response = requests.get(api_url)
