a5ba9b3be250e6f3dc14e6239d6c72e09b4a0089
hbk1087 <hansarianeil@gmail.com>
2023-10-19 20:36:23 -0400

    idek

--- backend/odds.py
+++ backend/odds.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 secret_key = os.getenv('API_KEY')
 # Replace this URL with the actual API URL you want to call
-api_url = f"https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds/?apiKey={secret_key}&regions=us&markets=h2h,spreads&oddsFormat=american"
+api_url = f"https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds/?apiKey={secret_key}&regions=us&markets=h2h,spreads,totals&oddsFormat=american"
 print(api_url)
 # Send a GET request to the API
 response = requests.get(api_url)
@@ -22,6 +22,10 @@ print(json_formatted_str)
 if response.status_code == 200:
     # Print the response data as text
     print("Response data:")
+    f = open('data.txt', 'w')
+    f.write(json_formatted_str)
+    f.close()
+
     #print(response.text)
 else:
     print(f"Request failed with status code {response.status_code}")
